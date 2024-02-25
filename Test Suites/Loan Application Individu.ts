88a892f95326d9645165ea631d50100a54462f4d
rizkyasyari <rizkyasyari7@gmail.com>
2024-02-25 21:47:05 +0700

    initiate commit

---
+++ Test Suites/Loan Application Individu.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Loan Application Individu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e0e05cf6-6ce9-42a9-9eee-cc50fc80b101</testSuiteGuid>
+   <testCaseLink>
+      <guid>5d7e5313-d1fd-4b07-a542-0e8346c8ce09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Individu/Business Capital Financing - Loan Application Individu</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
